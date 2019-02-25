ee30db7ec127b2f9cb1aca45d4f2c5ca70b6fad0
DapperZeus <33703306+DapperZeus@users.noreply.github.com>
2019-02-25 15:00:53 -0700

    commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace LuckySevens
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             int numberSevens = 0;
+            Random rng = new Random();
+
+            int dice1;
+            int dice2;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                dice1 = rng.Next(1, 7);
+                dice2 = rng.Next(1, 7);
+
+                if (dice1 + dice2 == 7)
+                numberSevens++;
+            }
+
+            Console.WriteLine("We rolled 100 times. Out of that 100 times, you rolled {0} sevens", numberSevens);
         }
     }
 }
